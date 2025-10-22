baea6b96813c3cf687f1b07a477304e958d43e26
Øystein Bringsli <oysteinmb@gmail.com>
2025-10-22 11:15:48 +0200

    Many update

---
+++ include/vision/Vision.hpp
@@ -0,0 +1,35 @@
+
+#ifndef AIS2203_JETRACER_VISION_HPP
+#define AIS2203_JETRACER_VISION_HPP
+
+#include <vector>
+#include <thread>
+
+#include <opencv2/opencv.hpp>
+#include <simple_socket/TCPSocket.hpp>
+#include <simple_socket/SimpleConnection.hpp>
+#include <iostream>
+
+class Vision {
+public:
+    Vision();
+    ~Vision();
+
+    bool isOkay;
+    void update();
+    cv::Mat getFrame();
+
+private:
+    cv::VideoCapture _cap;
+    cv::Mat _frame;
+
+    simple_socket::TCPServer _server;
+    std::thread _serverThread;
+    std::vector<std::thread> _connectionThreads;
+
+    void socketHandler(std::unique_ptr<simple_socket::SimpleConnection> conn);
+
+};
+
+
+#endif //AIS2203_JETRACER_VISION_HPP
--- tests/vision/testVision.cpp
+++ tests/vision/testVision.cpp
@@ -1,21 +1,18 @@
 
-
 #include <opencv2/opencv.hpp>
-#include <iostream>
+#include "vision/Vision.hpp"
+
+
 
 int main() {
-    std::string pipeline = "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, framerate=30/1 ! "
-                           "nvvidconv ! videoconvert ! appsink";
-    cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
-    if(!cap.isOpened()) {
-        std::cerr << "Failed to open CSI camera\n";
-        return -1;
-    }
-    cv::Mat frame;
+
+    Vision vision;
+
     while(true) {
-        cap >> frame;
+        vision.update();
+        auto frame = vision.getFrame();
         if(frame.empty()) break;
-        imshow("CSI Camera", frame);
+        cv::imshow("CSI Camera", frame);
         if(cv::waitKey(1) == 27) break; // ESC to exit
     }
     return 0;
