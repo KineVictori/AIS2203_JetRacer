99d705675b1fe957f6e4fd8b07963b4287f94d8b
Øystein Bringsli <oysteinmb@gmail.com>
2025-10-24 08:58:21 +0200

    Better connection shutdown

--- src/vision/Vision.cpp
+++ src/vision/Vision.cpp
@@ -53,15 +53,22 @@ void Vision::socketHandler(std::unique_ptr<simple_socket::SimpleConnection> conn
         mode = 1;
     } // TODO: implement mode switching (example AUTO: frames as fast as possible, one: just get one frame)
 
-    while (!_stopFlag) {
-        auto frame = getFrame();
-        std::vector<uchar> buf;
-        cv::imencode(".jpg", frame, buf);
+    try {
+        while (!_stopFlag) {
+            auto frame = getFrame();
+            std::vector<uchar> buf;
+            cv::imencode(".jpg", frame, buf);
 
-        int numBytes = buf.size();
-        conn->write(reinterpret_cast<char*>(&numBytes), sizeof(numBytes)); // send size as int
-        conn->write(reinterpret_cast<char*>(buf.data()), buf.size());      // send raw bytes
+            int numBytes = buf.size();
+            conn->write(reinterpret_cast<char*>(&numBytes), sizeof(numBytes)); // send size as int
+            conn->write(reinterpret_cast<char*>(buf.data()), buf.size());      // send raw bytes
+        }
+    } catch (...) {
+        // Probably conn has closed from client side, should probably do this in a better way.
+        // Could probably get client to send a msg "END" or whatever, then close.
+        // TODO: Handle better connection shutdown.
     }
+
 }
 
 bool Vision::isFinished() {
